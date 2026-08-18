3a0ae20feea7e0feec1a98870e7b7d4c19ef8c83
Qi Xuan <132574333+YourHand29@users.noreply.github.com>
2026-08-18 22:04:23 +0800

    Update Duke to YourHand

---
+++ src/main/java/YourHand.java
@@ -0,0 +1,14 @@
+public class YourHand {
+    public static void main(String[] args) {
+        String banner = "__   __                 _   _                 _\n"
+                + "\\ \\ / /__  _   _ _ __  | | | | __ _ _ __   __| |\n"
+                + " \\ V / _ \\| | | | '__| | |_| |/ _` | '_ \\ / _` |\n"
+                + "  | | (_) | |_| | |    |  _  | (_| | | | | (_| |\n"
+                + "  |_|\\___/ \\__,_|_|    |_| |_|\\__,_|_| |_|\\__,_|";
+        System.out.println(banner);
+
+        System.out.println("Selamat Datang 早上好! YourHand 为你服务");
+        System.out.println("你来这干嘛 What are you here for?");
+        System.out.println("See you never :)");
+    }
+}
